58caa57503837a0801a5236cb334d3548d1a178f
Gilvan OLiveira Silva <gilvanguru@gmail.com>
2022-06-22 15:44:45 -0300

    Entrada de dados via inpunt

---
+++ entrada_dados.py
@@ -0,0 +1,5 @@
+nome = str(input('Digite seu nome: '))
+idade = float(input('Digite seu idade: '))
+peso = float(input('Digite seu peso Kg: '))
+
+print(f'Bom dia!  {nome} você tem {idade} anos e pesa {peso} Kg')
--- saida_dados.py
+++ saida_dados.py
@@ -1,9 +1,9 @@
 #Aqui são algumas saidas de dados
 
-""" nome = input('Digite seu nome: ')
-peso = int(input('Digite seu peso: '))
+nome = input('Digite seu nome: ')
+peso = int(input('Digite seu peso Kg: '))
 idade = int(input('Digite sua idade: '))
-print(f'Sua idade é de {idade} anos, seu peso é {peso} Kg e seu nome é {nome}') """
+print(f'Sua idade é de {idade} anos, seu peso é {peso} Kg e seu nome é {nome}')
 
 #Aqui são separadores para utilizar no dia a dia separador sep=
 """ print('02', "11", '2021', sep='/')
@@ -12,8 +12,8 @@ print('B','n','n','.', sep='a')
 print('p','r','d','', sep='a') """
 
 #Final de texto como se escreve na mesma linha com o separador end=, 
-print('Vamos estudar na ', end=' ')
+""" print('Vamos estudar na ', end=' ')
 print('SoulCode Academy.')
 
 print('Preço ', end='R$ ')
-print(50, 'reais')
+print(50, 'reais') """
