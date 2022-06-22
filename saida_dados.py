8634e6cd1530f51c532e12dbab2972e7fc6f97ab
Gilvan OLiveira Silva <gilvanguru@gmail.com>
2022-06-22 14:50:43 -0300

    Saida de dados com end sep para utilizar na escrita do print

---
+++ saida_dados.py
@@ -0,0 +1,19 @@
+#Aqui são algumas saidas de dados
+
+""" nome = input('Digite seu nome: ')
+peso = int(input('Digite seu peso: '))
+idade = int(input('Digite sua idade: '))
+print(f'Sua idade é de {idade} anos, seu peso é {peso} Kg e seu nome é {nome}') """
+
+#Aqui são separadores para utilizar no dia a dia separador sep=
+""" print('02', "11", '2021', sep='/')
+print('2021', '11', '23', sep='-')
+print('B','n','n','.', sep='a')
+print('p','r','d','', sep='a') """
+
+#Final de texto como se escreve na mesma linha com o separador end=, 
+print('Vamos estudar na ', end=' ')
+print('SoulCode Academy.')
+
+print('Preço ', end='R$ ')
+print(50, 'reais')
