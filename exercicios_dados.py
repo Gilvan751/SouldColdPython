7eb7c9637e186f970b4c18d202cd789b7f4bdeab
Gilvan OLiveira Silva <gilvanguru@gmail.com>
2022-06-23 11:05:19 -0300

    Exercicios sobre dados

---
+++ exercicios_dados.py
@@ -0,0 +1,4 @@
+nome = str(input('Digite seu nome '))
+altura = float(input('Digite sua altura em cm: '))
+cpf = int(input('Digite seu CPF  '))
+print(f'Bom dia {nome} você tem de altura {altura/100} metros e seu CPF é {cpf}')
