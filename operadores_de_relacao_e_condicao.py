83bca399302e595a89976a9cd92634187547268c
Gilvan OLiveira Silva <gilvanguru@gmail.com>
2022-06-24 10:50:11 -0300

    Condição de if, elif, e else para verificação de variaveis

--- operadores_de_relacao_e_condicao.py
+++ operadores_de_relacao_e_condicao.py
@@ -2,8 +2,32 @@
 # == , !=
 #operadores relacionais
 #>,<,>=,<=
-numero = 100 >= 100
+""" numero = 100 >= 100
 print(numero)
-print(type(numero))
+print(type(numero)) """
+
+#if e else
+""" numero = 15
+if numero == 10:
+    print('Esse numero é igual a 10!')
+elif numero ==11:
+    print('Este numero é igual a 11!')
+elif numero ==12:
+    print('Este numero é igual a 12!')
+elif numero ==13:
+    print('Este numero é igual a 13!')
+else:
+    print('Esse numero não é igual a 10,11,12,13!') """
+
+#Exercicio
+numero = 12
+if numero == 15:
+    print('Esse número é igual a 15!')
+elif numero > 15:
+    print('Esse número é maiaor que  15!')
+elif numero < 15:
+    print('Esse número é menor que  15!')
+else:
+    print('Esse número é  não igual a 15!')
 
 
