64661faa4c4bedf78332663a060e27e73bed49b0
Gilvan OLiveira Silva <gilvanguru@gmail.com>
2022-06-24 11:18:37 -0300

    Utilização de while, for

--- estrutura_de_controle_de_fluxo.py
+++ estrutura_de_controle_de_fluxo.py
@@ -17,10 +17,33 @@ for i in 'PYTHON':
 num = [1,5,7,9,13,15]
 for j in num:
     print(j) """
-for i in range(10):
+""" for i in range(10):
     i+=1
     print(i)
 
 print('=-='*30)
 for j in range(-10,0,2):
-    print(j)
+    print(j) """
+
+#Exercicio imprimir os multiplos de 3 no intervalo de 0 a 15
+""" for i in range(0,18,3):
+    print(i) """
+""" 
+from time import sleep
+
+
+print('Entrando no laço....')
+for i in range(10):
+    if(i==5):
+        break
+    
+print('Saindo no laço....') """
+
+print('Entrando no laço...')
+i = 0
+while(i<10):
+    i+=1
+    if(i%2):
+        continue
+    print(i)
+print('Saindo do laço...')
