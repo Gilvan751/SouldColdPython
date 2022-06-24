34bb4383410490976567140777cec752fa7113a7
Gilvan OLiveira Silva <gilvanguru@gmail.com>
2022-06-24 11:06:24 -0300

    Comando for

--- estrutura_de_controle_de_fluxo.py
+++ estrutura_de_controle_de_fluxo.py
@@ -1,6 +1,6 @@
 #while == enquanto i=0 e um contador
 
-from time import sleep
+""" from time import sleep
 
 
 i = 0
@@ -8,3 +8,12 @@ while(i<=10):
     i +=1
     print("O loop esta rodando")
     sleep(1)
+ """
+
+i = 0
+for i in 'PYTHON':
+    print(f'index.{i}')
+
+num = [1,5,7,9,13,15]
+for j in num:
+    print(j)
