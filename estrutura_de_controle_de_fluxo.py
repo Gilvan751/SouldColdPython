94b858d2967ea44c1310d87b85ec00f7cfdbfa79
Gilvan OLiveira Silva <gilvanguru@gmail.com>
2022-06-24 11:09:39 -0300

    for in range

--- estrutura_de_controle_de_fluxo.py
+++ estrutura_de_controle_de_fluxo.py
@@ -10,10 +10,17 @@ while(i<=10):
     sleep(1)
  """
 
-i = 0
+""" i = 0
 for i in 'PYTHON':
     print(f'index.{i}')
 
 num = [1,5,7,9,13,15]
 for j in num:
+    print(j) """
+for i in range(10):
+    i+=1
+    print(i)
+
+print('=-='*30)
+for j in range(-10,0,2):
     print(j)
