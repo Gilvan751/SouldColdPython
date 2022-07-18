29ddee8e51039bf37e20c219864a7608bc9f7546
Gilvan751 <83382196+Gilvan751@users.noreply.github.com>
2022-07-18 17:13:25 -0300

    propriedades de strings

---
+++ aula02_propriedadesdasstrings.py
@@ -0,0 +1,12 @@
+l = "lista de letras"
+data = '18/07/2022'
+#propriedade do tamanho da string
+tamanho = len(l)
+print(tamanho)
+# divisao da string split
+lista = l.split(' ')
+lista2 = data.split('/')
+print(lista)
+print(lista2)
+#substituição de string
+print(l.replace('de', ' '))
